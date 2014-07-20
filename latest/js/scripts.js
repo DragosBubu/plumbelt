07e94269626b1f1dac019be1a48aedc378df84f4
Marius Cristea <cristea.marius.alexandru@gmail.com>
2014-07-20 07:15:41 -0500

    Merge development with production

---
+++ latest/js/scripts.js
@@ -0,0 +1,55 @@
+/*
+** Limit menu number of lists
+*/
+var full_width = 0;
+
+jQuery(".container ul:first > li").each(function( index ) {
+	if((jQuery(this).width() + full_width) > 750) {
+		jQuery(this).remove();
+	}
+	full_width = full_width + jQuery(this).width();
+});
+
+/*
+** FancyBox
+*/
+jQuery(document).ready(function($) {
+	$(".fancybox").fancybox();
+});
+
+/*
+** Masonry
+*/
+jQuery(document).ready(function($) {
+
+	var $container = $('.gallery');
+
+	$container.imagesLoaded( function(){
+		$container.masonry({
+			itemSelector : 'dl.gallery-item'
+		});
+	});
+});
+
+/*
+** Masonry
+*/
+jQuery(document).ready(function($) {
+
+	var $container = $('.testimonial-container');
+
+	$container.imagesLoaded( function(){
+		$container.masonry({
+			itemSelector : '.testimonial'
+		});
+	});
+});
+
+/*
+** Responsive Menu
+*/
+jQuery(document).ready(function($) {
+	$('.openresponsivemenu').click(function() {
+		$('nav').toggleClass("responsivemenu");
+	});
+});
---
+++ latest/page-testimonials.php
@@ -0,0 +1,55 @@
+<?php
+/*
+* 	The template for displaying Page Testimonials.
+*
+* 	@package ThemeIsle
+*
+*	Template Name: Testimonials
+*/
+get_header();
+?>
+<section class="content">
+	<div class="container">
+
+		<div id="testimonials" class="cf">
+				<h2><?php the_title(); ?></h2>
+
+				<?php
+
+				$args = array (
+					'post_type'              => 'testimonials',
+					'posts_per_page'         => '16',
+				);
+
+				$wp_query = new WP_Query( $args );
+
+				if ( $wp_query->have_posts() ) {
+					while ( $wp_query->have_posts() ) {
+						$wp_query->the_post();
+						$featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+
+						<div class="testimonial">
+							<?php
+							if ( $featured_image ) { ?>
+								<div class="img" style="background-image: url('<?php echo $featured_image[0]; ?>')"></div>
+							<?php }
+							?>
+							<h4><?php the_title(); ?></h4>
+							<div class="testimonial-content">
+								<?php echo excerpt_limit_length(get_the_excerpt(), '25'); ?>
+							</div><!--/.testimonial-content-->
+						</div><!--/.testimonial-->
+
+					<?php }
+				} else {
+					echo __( 'No posts found', 'ti' );
+				}
+
+				wp_reset_postdata();
+				?>
+
+			</div><!--/#testimonials-->
+
+	</div><!--/.container-->
+</section><!--/.content-->
+<?php get_footer(); ?>
